7683ae030474c8b4880970abf34b083949883a68
pochii-cloud <johnmwanikig2000@gmail.com>
2024-01-11 22:17:17 +0300

    added register component

--- swift-essay/src/App.js
+++ swift-essay/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Login } from './components/login/Login';
+import { Register } from './components/register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
     </div>
   );
 }
