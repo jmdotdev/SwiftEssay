1ce275e8bf2678e343ef47794591e52f949e09c4
pochii-cloud <johnmwanikig2000@gmail.com>
2024-01-15 07:51:20 +0300

    added sidenav-dashboard props

--- swift-essay/src/components/dashboard/Dashboard.js
+++ swift-essay/src/components/dashboard/Dashboard.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Dashboard.css'
 import { SideNav } from '../../components/sidenav/SideNav'
 import { TopNav } from "../topnav/TopNav";
 export const Dashboard = () => {
+  const[selectedLink,setSelectedLink] = useState(null);
+  
+  const handleSelectedLink = (link) =>{
+      setSelectedLink(link)
+  }
+ 
   return (
     <div className="dashboard">
       <div className="sidenav">
-        <SideNav/>
+        <SideNav onLinkClick={handleSelectedLink}/>
       </div>
       <div className="container">
         <div className="top-nav">
           <TopNav/>
         </div>
         <div className="container-content">
-          <p>content</p>
+          <p>{selectedLink}</p>
         </div>
       </div>
     </div>
--- swift-essay/src/components/sidenav/SideNav.js
+++ swift-essay/src/components/sidenav/SideNav.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import './SideNav.css'
 import notepad from '../../assets/images/notepad.png'
-export const SideNav = () => {
+export const SideNav = ({onLinkClick}) => {
+
+  const handleSelectedLink = (link) =>{
+    onLinkClick(link)
+  }
   return (
     <div className='sidenav'>
      <div className='sidenav-header'>
@@ -9,13 +13,13 @@ export const SideNav = () => {
             <b><h3>SwiftEssay</h3></b>
      </div>
       <ul>
-        <li>Dashboard</li>
-        <li>Writers</li>
-        <li>Orders</li>
-        <li>Alerts</li>
-        <li>Payments</li>
-        <li>Reviews</li>
-        <li>News</li>
+        <li onClick={() => {handleSelectedLink("dashboard")}}>Dashboard</li>
+        <li onClick={() => {handleSelectedLink("Writers")}}>Writers</li>
+        <li onClick={() => {handleSelectedLink("Orders")}}>Orders</li>
+        <li onClick={() => {handleSelectedLink("Alerts")}}>Alerts</li>
+        <li onClick={() => {handleSelectedLink("Payments")}}>Payments</li>
+        <li onClick={() => {handleSelectedLink("Reviews")}}>Reviews</li>
+        <li onClick={() => {handleSelectedLink("News")}}>News</li>
       </ul>
     </div>
   )
