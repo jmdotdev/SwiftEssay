7f3eaaaec45a1abca460b1edb769359eb096932c
pochii-cloud <johnmwanikig2000@gmail.com>
2024-01-19 08:23:00 +0300

    updated dahsboard icons

--- swift-essay/src/components/dashboard-content/DashboardContent.js
+++ swift-essay/src/components/dashboard-content/DashboardContent.js
@@ -3,13 +3,16 @@ import './DashboardContent.css'
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { HiOutlineShoppingBag } from "react-icons/hi2";
+import pendingIcon from '../../assets/images/pending.svg'
+import progressIcon from '../../assets/images/progress.jpg'
+import completedIcon from '../../assets/images/complete.jpg'
+import repeatIcon from '../../assets/images/repeat.svg'
 export const DashboardContent = () => {
 
-    const [value, setValue] = useState(0);
+    const [tabvalue, setTabValue] = useState(0);
 
     const handleChange = (event, newValue) => {
-      setValue(newValue);
+      setTabValue(newValue);
     };
   return (
     <div className='dashboard-content'>
@@ -23,45 +26,49 @@ export const DashboardContent = () => {
                    <h4>Pending Orders</h4>
                 </div>
                 <div className='right-section'>
-                   <HiOutlineShoppingBag/>
+                   <img src={pendingIcon} alt='orders-image.png'/>
                 </div>
             </div>
             <div className='dashboard-card'>
                 <div className='left-section'>
                    <h2>26</h2>
-                   <h4>Pending Orders</h4>
+                   <h4>Orders Inprogress</h4>
                 </div>
                 <div className='right-section'>
-                   <HiOutlineShoppingBag/>
+                   <img src={progressIcon} alt='orders-image.png'/>
                 </div>
-            </div>
-            <div className='dashboard-card'>
+            </div> <div className='dashboard-card'>
                 <div className='left-section'>
                    <h2>26</h2>
-                   <h4>Pending Orders</h4>
+                   <h4>Completed Orders</h4>
                 </div>
                 <div className='right-section'>
-                   <HiOutlineShoppingBag/>
+                   <img src={completedIcon} alt='orders-image.png'/>
                 </div>
-            </div>
-            <div className='dashboard-card'>
+            </div> <div className='dashboard-card'>
                 <div className='left-section'>
                    <h2>26</h2>
-                   <h4>Pending Orders</h4>
+                   <h4>Revision Orders</h4>
                 </div>
                 <div className='right-section'>
-                   <HiOutlineShoppingBag/>
+                   <img src={repeatIcon} alt='orders-image.png'/>
                 </div>
             </div>
         </div>
         <div className='tabs'>
         <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+      <Tabs value={tabvalue} onChange={handleChange} centered>
+        <Tab label="Approval Comments" />
+        <Tab label="Revision Requests" />
+        <Tab label="Order Disputes" />
       </Tabs>
     </Box>
+    <div className='tabs-info'>
+      {tabvalue == 0 && <p>Tab one</p>}
+      {tabvalue == 1 && <p>Tab two</p>}
+      {tabvalue == 2 && <p>Tab three</p>}
+      
+    </div>
         </div>
     </div>
   )
