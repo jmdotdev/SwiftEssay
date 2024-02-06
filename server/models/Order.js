20848f215b46d24e4b8a1fb1fe4a14bebff32f81
pochii-cloud <johnmwanikig2000@gmail.com>
2024-02-06 09:18:49 +0300

    added user-order relationship in models

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const Schema = mongoose;
 
 const orderSchema = new Schema({
-
+  
 })
 const Order = mongoose.model('Order',orderSchema)
 
--- server/models/Writer.js
+++ server/models/Writer.js
@@ -2,49 +2,48 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-   username:{
-    type:String,
-    required:true,
-    unique:true
-   },
-   email:{
-    type:String,
-    required:true,
-    unique:true
-   },
-   phone:{
-    type:String,
-    required:true,
-    unique:true
-   },
-   role:{
-      type:String,
-      required:true,
-      enum:['admin','client','writer']
-   },
-   assigned_tasks:{
-      type:Array,
-      default:[]
-   },
-   posted_jobs:{
-      type:Array,
-      default:[]
-   },
-   paypal_account:{
-    type:String,
-    required:false
-   },
-   password:{
-    type:String,
-    required:true
-   },
-   is_assigned:{
-      type:Boolean,
-      default:false
-   },
-   timestamps:true
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "client", "writer"],
+  },
+  assigned_tasks: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Order", default: [] },
+  ],
+  posted_jobs: {
+    type: Array,
+    default: [],
+  },
+  paypal_account: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  is_assigned: {
+    type: Boolean,
+    default: false,
+  },
+  timestamps: true,
 });
 
-const User = mongoose.model('User',userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports(User)
+module.exports(User);
