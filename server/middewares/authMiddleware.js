4d18e9a75325c6d6d80e66e7167cfe55895d7c64
pochii-cloud <johnmwanikig2000@gmail.com>
2024-03-01 21:35:11 +0300

    fix:fixed unauthorized error in protected routes

--- server/middewares/authMiddleware.js
+++ server/middewares/authMiddleware.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.header("Authorization");
+  const token = req.headers.authorization.split(' ')[1];
+
   try {
     const decoded = jwt.verify(token, "mysecretkey");
-    console.log("decoded",decoded)
+    console.log("backend decoded",decoded)
     req.email = decoded.email;
     next();
   } catch (error) {
