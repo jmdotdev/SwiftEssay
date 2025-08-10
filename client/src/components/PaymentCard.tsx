cfc1a5e19e014823925755c3b5d62279e8e0cf6a
John Mwaniki <johnmwanikig2000@gmail.com>
2025-08-10 22:01:20 +0300

    refactor: refactor payment page

---
+++ client/src/components/PaymentCard.tsx
@@ -0,0 +1,13 @@
+export const PaymentCard = () => {
+  return (
+    <div className="flex items-center justify-between bg-white rounded-md p-4">
+        <div className="flex flex-col w-1/2">
+            <p>$500</p>
+            <p>Total Incom</p>
+        </div>
+        <div className="flex items-center justify-end w-1/2">
+          <img src="/images/money.webp"  className="h-20 w-20"/>
+        </div>
+    </div>
+  )
+}
